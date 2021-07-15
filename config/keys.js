496557f2a62070418a51a5aef28908de673703d4
Luke Gartland <lukegartland@gmail.com>
2021-07-15 11:01:05 -0500

    travis and keys config

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,5 @@
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'ci') {
   module.exports = require('./prod');
-} else if (process.env.NODE_ENV === 'ci') {
-  module.exports = require('./ci');
 } else {
   module.exports = require('./dev');
 }
