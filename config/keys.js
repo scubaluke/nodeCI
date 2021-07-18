1733d3e3c1ec91057a5a335137cab74c90ce12d5
Luke Gartland <lukegartland@gmail.com>
2021-07-18 09:29:37 -0500

    added aws s3 keys to dev not prod

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
-// if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'ci') {
-//   module.exports = require('./prod');
-// } else {
-//   module.exports = require('./dev');
-// }
-
-
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'ci') {
   module.exports = require('./prod');
+} else {
+  module.exports = require('./dev');
+}
+
+
+  // module.exports = require('./prod');
